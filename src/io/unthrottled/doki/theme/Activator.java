95e904f441741548dbb63c38bd18a7d369aad956
Alex Simons <alexsimons9999@gmail.com>
2020-08-08 19:12:21 -0500

    Start of the plugin view

---
+++ src/io/unthrottled/doki/theme/Activator.java
@@ -0,0 +1,22 @@
+package io.unthrottled.doki.theme;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
