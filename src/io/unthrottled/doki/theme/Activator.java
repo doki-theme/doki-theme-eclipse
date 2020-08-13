22bac20d794033f496be91add3d3a11abcae0cc9
Alex Simons <alexsimons9999@gmail.com>
2020-08-13 18:11:14 -0500

    And just like that I have the preferences

--- src/io/unthrottled/doki/theme/Activator.java
+++ src/io/unthrottled/doki/theme/Activator.java
@@ -1,14 +1,22 @@
 package io.unthrottled.doki.theme;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+public class Activator extends AbstractUIPlugin implements BundleActivator {
 
 	private static BundleContext context;
+	private static Activator activator;
 
 	static BundleContext getContext() {
 		return context;
+	}
+
+	public Activator() {
+		super();
+		activator = this;
 	}
 
 	public void start(BundleContext bundleContext) throws Exception {
@@ -17,6 +25,14 @@ public class Activator implements BundleActivator {
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+	}
+	
+	public static Activator getDefault() {
+		return activator;
+	}
+	
+	protected void initializeDefaultPreferences(IPreferenceStore store) {
+		System.out.println("Finna default");
 	}
 
 }
