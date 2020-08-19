ee4490f724c1a3f076d38fa37d1b7211f34389f7
Alex Simons <alexsimons9999@gmail.com>
2020-08-19 04:50:47 -0500

    Switcho Dynamo

--- src/io/unthrottled/doki/theme/Activator.java
+++ src/io/unthrottled/doki/theme/Activator.java
@@ -1,18 +1,8 @@
 package io.unthrottled.doki.theme;
 
-import static io.unthrottled.doki.theme.preferences.PreferenceConstants.CURRENT_THEME_PREFERENCE;
-import static io.unthrottled.doki.theme.preferences.PreferenceConstants.STICKER_TYPE_PREFERENCE;
-
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-
-import io.unthrottled.doki.theme.themes.StickerService;
-import io.unthrottled.doki.theme.themes.ThemeManager;
 
 public class Activator extends AbstractUIPlugin implements BundleActivator {
 
@@ -30,20 +20,6 @@ public class Activator extends AbstractUIPlugin implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
-		
-		var preferenceStore = getPreferenceStore();
-    preferenceStore.addPropertyChangeListener(new IPropertyChangeListener() {
-      @Override
-      public void propertyChange(PropertyChangeEvent changeEvent) {
-        if (CURRENT_THEME_PREFERENCE.equals(changeEvent.getProperty())) {
-        	ThemeManager.getInstance().getTheme((String)changeEvent.getNewValue())
-        	.ifPresent(dokiTheme -> {        		
-        		PlatformUI.getWorkbench().getThemeManager().setCurrentTheme(
-        				dokiTheme.getCSSId());
-        	});;
-        }
-      }
-    });
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
--- src/io/unthrottled/doki/theme/parts/StickerView.java
+++ src/io/unthrottled/doki/theme/parts/StickerView.java
@@ -2,12 +2,16 @@ package io.unthrottled.doki.theme.parts;
 
 import io.unthrottled.doki.theme.Activator;
 import io.unthrottled.doki.theme.themes.StickerService;
+import io.unthrottled.doki.theme.themes.ThemeManager;
+
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.PlatformUI;
 
 import javax.annotation.PostConstruct;
 
@@ -18,16 +22,24 @@ public class StickerView {
   private Label stickerDisplayLabel;
 
   @PostConstruct
-  public void createPartControl(Composite parent) {
+  public void createPartControl(Composite parent, IThemeEngine themeEngine) {
     var preferenceStore = Activator.getDefault().getPreferenceStore();
     preferenceStore.addPropertyChangeListener(new IPropertyChangeListener() {
       @Override
       public void propertyChange(PropertyChangeEvent changeEvent) {
-        if (CURRENT_THEME_PREFERENCE.equals(changeEvent.getProperty()) ||
+        boolean themeChanged = CURRENT_THEME_PREFERENCE.equals(changeEvent.getProperty());
+				if (themeChanged ||
             STICKER_TYPE_PREFERENCE.equals(changeEvent.getProperty())) {
           var imageUrl = StickerService.getInstance().getCurrentStickerUrl();
           stickerDisplayLabel.setImage(new Image(parent.getDisplay(), imageUrl));
         }
+				
+				if(themeChanged) {
+					ThemeManager.getInstance().getTheme((String)changeEvent.getNewValue())
+        	.ifPresent(dokiTheme -> {        
+        		themeEngine.setTheme(dokiTheme.getCSSId(),true);
+        	});
+				}
       }
     });
     stickerDisplayLabel = new Label(parent, SWT.BORDER);
